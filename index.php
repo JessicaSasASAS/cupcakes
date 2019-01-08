19e52feff5970f07bede7ebf515b3a53c7b2c259
Jessica Sas <jsas2@mail.greenriver.edu>
2019-01-07 19:39:12 -0500

    Created flavor check boxes and submit button

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Cupcakes</title>
+</head>
+<body>
+<h1>Cupcakes!</h1>
+<form>
+    <fieldset>
+        <legend>Flavors:</legend>
+        <input type="checkbox" name="flavor1" value="Vanilla"> Vanilla<br>
+        <input type="checkbox" name="flavor2" value="Chocolate"> Chocolate<br>
+        <input type="checkbox" name="flavor3" value="Carrot Cake"> Carrot Cake<br>
+    </fieldset>
+    <input type="button" value="Submit Order">
+</form>
+
+</body>
+</html>
